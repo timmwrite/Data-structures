fa02bcabfd21ea88ce29b9421c5b8e68b4b9b1cf
Timm Ndirangu <timmndirangu1@gmail.com>
2019-07-30 18:22:35 +0300

    Update triplets.py

--- triplets.py
+++ triplets.py
@@ -3,6 +3,21 @@ elements = [12, 3, 1, 2, -6, 5, -8, 6]
 target  = 0
 
 def check_pairs(elements, target = 0):
+    
+     """ 
+            Check_smallest_list1 checks the smallest item in list1.
+
+            Par:
+                List1 is a list of iterable items.
+
+            Returns:
+                    Appends the smallest item to a new list (Pair).
+
+            Run Time: 
+                    Linear Time — O(n)
+                    
+        
+        """
 
     check_index = {}
     for i, x in range(len(elements)):
